af935648da511c96200fe2edb3c42440e2a6de9e
harkpro <harkpro@users.noreply.github.com>
2020-06-14 12:22:13 +0700

    thêm template

---
+++ admin/template.php
@@ -0,0 +1,49 @@
+<?php 
+$path = "../";
+require_once $path.$path.'commons/utils.php';
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>AdminLTE 2 | Dashboard</title>
+  <?php include_once $path.'_share/style_assets.php'; ?>
+</head>
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+  <?php include_once $path.'_share/header.php'; ?>
+  
+  <?php include_once $path.'_share/sidebar.php'; ?>
+  
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Dashboard
+        <small>Control panel</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Dashboard</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      
+
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  
+  <?php include_once $path.'_share/footer.php'; ?>
+</div>
+<!-- ./wrapper -->
+
+<?php include_once $path.'_share/script_assets.php'; ?>
+</body>
+</html>
